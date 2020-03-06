d998eb48442915e46c3a43d7d832ec59f4155a7e
Slava <solovey.1999@mail.ru>
2020-03-07 03:59:33 +0700

    Second commit

--- src/lvl5.kt
+++ src/lvl5.kt
@@ -1,17 +1,19 @@
+import java.sql.DatabaseMetaData
+
 fun main(args: Array<String>) {
 
 
-   val table:MutableList<String> = mutableListOf()
+    val table: MutableList<String> = mutableListOf()
 
-    for(a in args) {
-        table.add(a+" "+args.count { it == a })
+    for (a in args) {
+        table.add(a + " " + args.count { it == a })
     }
     val dist = table.distinct()
 
-    val lengthComparator = compareByDescending<String> { it.substring(it.indexOf(' '),it.lastIndex) }
-    val lengthThenString = lengthComparator.thenBy { it.substring(0,it.indexOf(' ')) }
+    val lengthComparator = compareByDescending<String> { it.substring(it.indexOf(' '), it.lastIndex) }
+    val lengthThenString = lengthComparator.thenBy { it.substring(0, it.indexOf(' ')) }
 
-    for(a in dist.sortedWith(lengthThenString))
+    for (a in dist.sortedWith(lengthThenString))
         println(a)
 
 }
